8d0033a56c78e7ae796398cf898cbdb574203801
Zach Robinson <robiz7684@wrdsb.ca>
2019-05-20 18:44:53 -0400

    Completed

---
+++ ZachRU3A1Q4/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ZachRU3A1Q4
+//
+//  Created by Leysa on 2019-05-20.
+//  Copyright © 2019 Zach Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func UpButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func LeftButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func RightButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func DownButton(_ sender: UIButton) {
+        print("whatever")
+    }
+}
+
